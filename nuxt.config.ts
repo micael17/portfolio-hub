c3eb8f4e74ad6940bf97a2a3430b29e5102eef92
jihongkim <jihongkim@smilegate.com>
2026-07-06 18:30:26 +0900

    chore: add cloudflare-pages nitro preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
   app: {
     head: {
       title: '웹 포트폴리오 쇼케이스',
